a076e09146aee2587da179ebf784c25714ba3076
Eugene Krivosheev <eugene.krivosheev@gmail.com>
2021-09-16 09:41:01 +0300

    Update iaac: update app scripts.

--- iaac/roles/target_app/files/start-app.sh
+++ iaac/roles/target_app/files/start-app.sh
@@ -4,9 +4,10 @@ nohup \
   java \
     -Xms128m -Xmx128m \
     -XX:+IgnoreUnrecognizedVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions \
-    -XX:+PrintCompilation -XX:+LogCompilation -XX:LogFile=jit.log \
-    -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heapdump.hprof \
+    -XX:ReservedCodeCacheSize=100m \
+    -XX:+PrintCompilation -XX:+LogCompilation -XX:LogFile=jit.log.xml \
     -XX:+TraceClassLoading -XX:+TraceClassUnloading \
+    -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heapdump.hprof \
     -Xloggc:gc.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=8 -XX:GCLogFileSize=8m \
     -Xlog:gc*,safepoint:gc.log:time,uptime:filecount=10,filesize=128K \
     -XX:NativeMemoryTracking=detail \
