804f5307c624e1daccdd94b6c678eff2c8d09f2b
Eugene Krivosheev <eugene.krivosheev@gmail.com>
2021-09-18 22:20:07 +0300

    Update iaac: fix app startup script.

--- iaac/roles/target_app/files/start-app.sh
+++ iaac/roles/target_app/files/start-app.sh
@@ -2,19 +2,20 @@
 
 nohup \
   java \
-    -Xms128m -Xmx128m \
+    -Xms512m -Xmx1G \
     -XX:+IgnoreUnrecognizedVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions \
-    -XX:ReservedCodeCacheSize=100m \
-    -XX:+PrintCompilation -XX:+LogCompilation -XX:LogFile=jit.log.xml \
-    -XX:+TraceClassLoading -XX:+TraceClassUnloading \
+    -XX:ReservedCodeCacheSize=200m \
     -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heapdump.hprof \
-    -Xloggc:gc.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=8 -XX:GCLogFileSize=8m \
-    -Xlog:gc*,safepoint:gc.log:time,uptime:filecount=10,filesize=128K \
-    -XX:NativeMemoryTracking=detail \
     -Dderby.stream.error.file=log/derby.log \
     -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false \
     -Djava.rmi.server.hostname=127.0.0.1 -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.rmi.port=9999 \
     -jar dbo-1.0-SNAPSHOT.jar \
       --spring.profiles.active=qa \
-      --server.port=8080 \
+      --debug=false \
+      --logging.level.root=ERROR \
+      --logging.level.org.springframework=ERROR \
+      --logging.level.org.hibernate=ERROR \
+      --logging.level.org.hibernate.stat=ERROR \
+      --logging.level.org.hibernate.SQL=ERROR \
+      --spring.jpa.show-sql=false \
 > /dev/null 2>&1 &
